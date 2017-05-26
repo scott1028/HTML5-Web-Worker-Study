fde743bf6d93196f8be0ba92dde1c9d2518dc58d
scott.lan <scott.lan@taisys.com>
2017-05-26 14:40:34 +0800

    fm

---
+++ sample01_non_separate_js_file.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// JavaScript Multi-Thread 替代方案
+
+var worker = new Worker(window.URL.createObjectURL(new Blob([`
+    self.onmessage = function(e){
+        console.log(e);
+        self.postMessage('sent from worker');
+    };
+`], {type: 'text/javascript'})))
+
+worker.onmessage = function(e){
+    console.log('received from worker', e);
+};
+
+worker.postMessage('Hello World!');
